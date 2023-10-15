cfca0dc1fb3fa394b1f15a6c912ca47fe5e64124
Salma Ahmed <salmahamzaahmed77@gmail.com>
2023-10-15 17:03:37 +0300

    switch between tasks's pages

--- Django-project/tasks/urls.py
+++ Django-project/tasks/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+# This just helps uniquely identiy all of the URLs 
+app_name = "tasks"
+
 urlpatterns = [
     path("", views.index, name = "index"),
     path("add", views.add, name = "add")
