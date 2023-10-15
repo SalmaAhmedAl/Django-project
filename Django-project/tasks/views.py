58ad2e71aec4dd14d6a8beb93e5b2940bd2bbbea
Salma Ahmed <salmahamzaahmed77@gmail.com>
2023-10-15 09:41:13 +0300

    new app added [tasks]

---
+++ Django-project/tasks/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Global variable that the entire application gonna have access to.
+tasks = ["Foo", "Bar", "Baz"]
+# Create your views here.
+def index(request):
+     return render(request, "tasks/index.html", {
+        "tasks" : tasks
+     })
