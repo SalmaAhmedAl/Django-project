3d379ec0d4144ec21993f96163b65843ab2ea01e
Salma Ahmed <salmahamzaahmed77@gmail.com>
2023-10-16 04:10:44 +0300

    add submited task to all tasks!

--- Django-project/tasks/views.py
+++ Django-project/tasks/views.py
@@ -1,16 +1,19 @@
 from django import forms
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 # Global variable that the entire application gonna have access to.
-tasks = ["Foo", "Bar", "Baz"]
+tasks = []
 
 # This class will inherit from forms.Form
 # Inside this class I will define all fields I'd like for this field to have.
 # all of the inputs that I'd like the user to prodive.
 class NewTaskForm(forms.Form):
    task = forms.CharField(label="New Task ")
+   
    # Min & Max make client-server validation! The server isn't getting any of this data.
-   priority = forms.IntegerField(label="Priority", min_value=1, max_value=10 )
+   # priority = forms.IntegerField(label="Priority", min_value=1, max_value=10 )
 
 
 # Create your views here.
@@ -20,7 +23,20 @@ def index(request):
      })
 
 def add(request):
-    return render(request, "tasks/add.html", {
+   if request.method == "POST":
+      form = NewTaskForm(request.POST)
+      # Server validation
+      if form.is_valid():
+         task = form.cleaned_data["task"]
+         tasks.append(task)
+         return HttpResponseRedirect(reverse("tasks:index"))
+      else:
+         return render(request, "tasks/add.html",{
+            "form" : form
+         })
+
+   
+   return render(request, "tasks/add.html",{
       # I'm gonna create a new task form, pass it into this add.html template.
       "form" : NewTaskForm()
-    })
+   })
